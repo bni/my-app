438011bb940a72a10693f09d5928930a3cdeb07f
bni <bjorn.nilsson@tiqqe.com>
2019-03-29 13:42:42 +0100

    next js

--- cow.ts
+++
@@ -1,9 +0,0 @@
-import { IncomingMessage, ServerResponse } from "http";
-
-const handler = (req: IncomingMessage, res: ServerResponse) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-
-    res.end(JSON.stringify({cow: "Moooo!"}));
-};
-
-export default handler;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function Index() {
+  return (
+    <div>
+      <h1>Hello Tiqqe 👋</h1>
+      <Link href="/about"><a>About</a></Link>
+    </div>
+  )
+};
